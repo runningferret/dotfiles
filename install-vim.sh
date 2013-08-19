17ae60d0b795d3750cee5c12d6417cafb1fd8630
Tony Castiglione <tcastiglione@gmail.com>
2013-08-18 21:37:25 -0500

    Better handle non-system rubies rvm is weird about login shells... should probably use rbenv everywhere

--- install-vim.sh
+++ install-vim.sh
@@ -1,10 +1,24 @@
-#!/bin/sh
+#!/bin/bash --login
 #Shamlessly stolen from https://github.com/vgod/vimrc
 cd ~
 ls -s dotfiles/.vim .vim 
+if [ -e .vimrc ]
+then
+  mv .vimrc .vimrc.backup
+  echo "moved existing .vimrc to .vimrc.backup"
+fi
+if [ -e .gvimrc ]
+then
+  mv .gvimrc .gvimrc.backup
+  echo "moved existing .gvimrc to .gvimrc.backup"
+fi
+
 ln -s dotfiles/.vim/vimrc .vimrc 
 ln -s dotfiles/.vim/gvimrc .gvimrc 
+
 yes | vim +BundleInstall +qall
 cd ~/.vim/bundle/command-t/ruby/command-t
+rvm use system #Needs a login shell. Should be using rbenv
 ruby extconf.rb
+make clean
 make
