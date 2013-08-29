96dc523d1c7cf55163e4b89f29ab9bc86c36e916
Tony Castiglione <tcastiglione@gmail.com>
2013-08-28 20:58:02 -0500

    Add rbenv installer Automate rbenv installation, update gitignore for pesky swap files

---
+++ install-rbenv.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+RBENV_EVAL='eval "$(rbenv init -)"'
+RBENV_VERSION='1.9.3-p448'
+
+if brew list | grep -q 'rbenv'
+then
+  brew upgrade rbenv
+else
+  brew install rbenv
+fi
+
+if brew list | grep -q 'ruby-build'
+then
+  brew upgrade ruby-build
+else
+  brew install ruby-build
+fi
+
+if grep -q "$RBENV_EVAL" ~/.bash_profile
+then
+  echo "Skipping... "$RBENV_EVAL" already in bash_profile"
+else
+  echo $RBENV_EVAL >> ~/.bash_profile
+fi
+
+if rbenv versions | grep -q "$RBENV_VERSION"
+then
+  echo "$RBENV_VERSION already installed!"
+else
+  echo "******************************************"
+  echo "Running: $RBENV_VERSION... This is gonna take a while, be patient"
+  echo "******************************************"
+  `$RBENV_VERSION`
+fi
