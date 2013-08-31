4f1a7091deedf01f52ac7a4e5f2570aabf093799
Tony Castiglione <tcastiglione@gmail.com>
2013-08-31 14:44:11 -0500

    General cleanup

--- install-tmuxconf.sh
+++ install-tmuxconf.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
 
 cd ~
+if [ -e .tmux.conf ]
+then
+  mv .tmux.conf .tmux.conf.backup
+  echo "Moved existing .tmux.conf to .tmux.conf.backup"
+fi
+
 ln -s dotfiles/tmux.conf .tmux.conf
+echo "Installed tmuxconf!"
--- install-vim.sh
+++ install-vim.sh
@@ -1,20 +1,15 @@
 #!/bin/bash
 #Shamlessly stolen from https://github.com/vgod/vimrc
 cd ~
-ls -s dotfiles/.vim .vim 
+ls -s dotfiles/vim .vim 
 if [ -e .vimrc ]
 then
   mv .vimrc .vimrc.backup
-  echo "moved existing .vimrc to .vimrc.backup"
-fi
-if [ -e .gvimrc ]
-then
-  mv .gvimrc .gvimrc.backup
-  echo "moved existing .gvimrc to .gvimrc.backup"
+  echo "Moved existing .vimrc to .vimrc.backup"
 fi
 
-ln -s dotfiles/.vim/vimrc .vimrc 
-ln -s dotfiles/.vim/gvimrc .gvimrc 
+ln -s dotfiles/vim/vimrc .vimrc 
+echo "Installed .vimrc!"
 
 yes | vim +BundleInstall +qall
 cd ~/.vim/bundle/command-t/ruby/command-t
