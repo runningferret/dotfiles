9339bb0dad89872c849fb3bc96e8a15e4fd0e95c
Tony Castiglione <runningferret@gmail.com>
2013-11-04 10:28:40 -0600

    Update globals

--- install-git.sh
+++ install-git.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 cd ~/dotfiles
 git remote set-url origin https://github.com/runningferret/dotfiles.git
+git config --global user.name "Tony Castiglione"
+git config --global user.email "runningferret@gmail.com"
