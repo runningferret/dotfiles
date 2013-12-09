4970af64f35312e6c26f218918b0dba0d89b7c15
Tony Castiglione <runningferret@gmail.com>
2013-12-09 13:13:46 -0600

    Update tmux more Update conf copy paste error. Update tmux.sh to use send keys to do what I really wanted to in the first place

--- tmux.sh
+++ tmux.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 COMMAND='tmux -S /var/tmux/pairing'
 $COMMAND new-session -d -s air -n "main"
+$COMMAND send-keys -t air 'cd ~/dev; clear' C-m
 
-$COMMAND splitw -d
-$COMMAND splitw -d
-$COMMAND select-layout main-horizontal
+$COMMAND splitw -t air -p 30
 
 $COMMAND new-window -t air -n "shell" -d
 
